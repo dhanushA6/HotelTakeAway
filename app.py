45aaeeb6a7e4f7c5cf08ec0d070d4afc0d70a866
henry-jacq <henryeditz267@gmail.com>
2023-12-20 21:37:13 +0530

    feat: new route category added

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('customer.html')
+    return render_template('index.html', page_name="Home")
+
+@app.route('/category/<name>', methods=['GET', 'POST'])
+def category(name):
+    return render_template('category.html', page_name="Category", name=name.capitalize())
 
 @app.route('/cart')
 def cart():
