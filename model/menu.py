6c18687eb7f3603416fd25e0ee9b1d3fad5a6ca6
Dhanushanadhan <dhanush2210530@ssn.edu.in>
2023-12-20 23:17:19 +0530

    menu file modified

--- model/menu.py
+++ model/menu.py
@@ -6,13 +6,6 @@ class MenuItem:
         self.description = description
         self.available = available
 
-class ItemDisplay:
-    def __init__(self, item_object):
-        self.item = item_object
-        self.name = item_object.name
-        self.price = item_object.price
-        self.description = item_object.description
-        self.available = item_object.available
     def display(self):
         print(f'{self.name}', ' '*(20-len(self.name)) ,
         f'{self.price}', ' '*(5-len(str(self.price))) ,
