7cf9ea0fd54830fc0f189a4559e2e30cf2bf9f53
henry-jacq <henryeditz267@gmail.com>
2023-12-21 09:10:27 +0530

    change: New attr added

--- model/menu.py
+++ model/menu.py
@@ -1,10 +1,11 @@
 #class to create the item
 class MenuItem:
-    def __init__(self, name, price, description, available = True):
+    def __init__(self, name, price, description, img, available=True):
         self.name = name
         self.price = price
         self.description = description
         self.available = available
+        self.img = img
 
     def display(self):
         print(f'{self.name}', ' '*(20-len(self.name)) ,
@@ -12,4 +13,4 @@ class MenuItem:
         f'{self.description}', ' '*(25- len(self.description)), f'{self.available}')
 
     def get_attrs(self):
-        return [self.name, self.price, self.description, self.available]
+        return [self.name, self.price, self.description,  self.img, self.available]
